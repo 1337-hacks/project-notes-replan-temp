7708d3b7622b215dfd5f03756d82b505e6c99087
1337-hacks <elijahnucum.exe@gmail.com>
2023-02-17 13:42:08 +1300

    c003 initialize firebase backend

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,23 +4,26 @@ export default function Header() {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="#">Project:NOTES</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="#">Project:NOTES</a>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <Link className="nav-link" href="/"><i className="bi bi-house"></i></Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" href="#"><i className="bi bi-journals"></i></Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" href="#"><i className="bi bi-gear"></i></Link>
-                </li>
-                </ul>
-            </div>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <Link className="nav-link" href="/">{/*<i className="bi bi-house"></i>*/}About</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link" href="#">{/*<i className="bi bi-journals"></i>*/}Blog</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className="nav-link" href="#">{/*<i className="bi bi-gear"></i>*/}Contact</Link>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+                        <Link className="nav-link" href="#">{/*<i className="bi bi-gear"></i>*/}Login</Link>
+                    </span>
+                </div>
             </div>
         </nav>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from '@next/font/google'
+import styles from '../styles/styles.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,14 +15,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className={styles.wrapper}>
         {/* Body */}
         <div className="container text-center h-100">
           <div className="row">
             <div>Welcome to Project:NOTES.</div>
             <div>Click here to start. <Link href="/workspace">Start</Link></div>
-          </div>
-          
+          </div>   
         </div>
 
         {/* Footer */}
